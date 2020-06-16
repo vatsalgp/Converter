04bf07b38f9cfe59e83a6a19016c385e36110a2a
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-16 16:30:50 +0530

    refactored more

--- Converter.java
+++ Converter.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class Converter {
-    private static StringBuilder sourceString = new StringBuilder();
-    private static StringBuilder destString = new StringBuilder();
-    private static int sourceRadix;
-    private static int destRadix;
+    private static class Number {
+        StringBuilder string = new StringBuilder();
+        int radix;
+    }
+
+    private static Number source = new Number();
+    private static Number dest = new Number();
 
     public static void main(final String[] args) {
         try {
@@ -15,55 +18,55 @@ public class Converter {
         }
     }
 
-    private static void printOutput() {
-        int integer = (int) Double.parseDouble(sourceString.toString());
-        if (destRadix == 1)
-            for (int i = 0; i < integer; i++)
-                destString.append("1");
-        else if (destRadix == 10)
-            destString.append((sourceString));
-        else {
-            destString.append(Integer.toString(integer, destRadix)).append(".");
-            double number = Double.parseDouble(sourceString.toString());
-            number -= integer;
-            for (int i = 0; i < 5; i++) {
-                number *= destRadix;
-                integer = (int) number;
-                number -= integer;
-                destString.append(Integer.toString(integer, destRadix));
-            }
-        }
-        System.out.println(destString);
-    }
-
     private static void getInput() throws Exception {
         final Scanner scanner = new Scanner(System.in);
         final String first = scanner.nextLine();
         final String second = scanner.nextLine();
         final String third = scanner.nextLine();
         scanner.close();
-        sourceRadix = getRadix(first);
-        destRadix = getRadix(third);
-        switch (sourceRadix) {
+        source.radix = getRadix(first);
+        dest.radix = getRadix(third);
+        switch (source.radix) {
             case 1:
-                sourceString.append(Integer.toString(second.trim().length()));
+                source.string.append(Integer.toString(second.trim().length()));
                 break;
             case 10:
-                sourceString.append(second);
+                source.string.append(second);
                 break;
             default:
                 final String[] parts = second.split("\\.");
-                double number = Integer.parseInt(parts[0], sourceRadix);
+                double number = Integer.parseInt(parts[0], source.radix);
                 if (parts.length > 1) {
                     int count = 1;
                     for (final String ch : parts[1].split("")) {
-                        final double d = Integer.parseInt(ch, sourceRadix);
-                        number += d / Math.pow(sourceRadix, count);
+                        final double d = Integer.parseInt(ch, source.radix);
+                        number += d / Math.pow(source.radix, count);
                         count++;
                     }
                 }
-                sourceString.append(Double.toString(number));
+                source.string.append(Double.toString(number));
         }
+    }
+
+    private static void printOutput() {
+        int integer = (int) Double.parseDouble(source.string.toString());
+        if (dest.radix == 1)
+            for (int i = 0; i < integer; i++)
+                dest.string.append("1");
+        else if (dest.radix == 10)
+            dest.string.append((source.string));
+        else {
+            dest.string.append(Integer.toString(integer, dest.radix)).append(".");
+            double number = Double.parseDouble(source.string.toString());
+            number -= integer;
+            for (int i = 0; i < 5; i++) {
+                number *= dest.radix;
+                integer = (int) number;
+                number -= integer;
+                dest.string.append(Integer.toString(integer, dest.radix));
+            }
+        }
+        System.out.println(dest.string);
     }
 
     private static int getRadix(String string) throws Exception {
