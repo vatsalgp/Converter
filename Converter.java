d5a4641703ca9ea3ef69906bbfcc5fb1a8036138
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-16 16:07:48 +0530

    refactored

--- Converter.java
+++ Converter.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Converter {
-    final private static Scanner scanner = new Scanner(System.in);
-    private static double num;
-    private static int radix;
+    private static double number;
+    private static int sourceRadix;
+    private static int destRadix;
 
     public static void main(final String[] args) {
         try {
@@ -16,62 +16,59 @@ public class Converter {
 
     private static void printOutput() {
         final StringBuilder out = new StringBuilder();
-        int integer = (int) num;
-        if (radix == 1)
+        int integer = (int) number;
+        if (destRadix == 1)
             for (int i = 0; i < integer; i++)
                 out.append("1");
-        else if (radix == 10)
-            out.append(Double.toString(num));
+        else if (destRadix == 10)
+            out.append(Double.toString(number));
         else {
-            out.append(Integer.toString(integer, radix)).append(".");
-            num -= integer;
+            out.append(Integer.toString(integer, destRadix)).append(".");
+            number -= integer;
             for (int i = 0; i < 5; i++) {
-                num *= radix;
-                integer = (int) num;
-                num -= integer;
-                out.append(Integer.toString(integer, radix));
+                number *= destRadix;
+                integer = (int) number;
+                number -= integer;
+                out.append(Integer.toString(integer, destRadix));
             }
         }
         System.out.println(out);
     }
 
     private static void getInput() throws Exception {
-        int sourceRadix = 0;
+        final Scanner scanner = new Scanner(System.in);
         final String first = scanner.nextLine();
-        if (validRadix(first))
-            sourceRadix = Integer.parseInt(first);
-        else
-            throw new Exception();
         final String second = scanner.nextLine();
-        if (sourceRadix == 1)
-            num = second.trim().length();
-        else if (sourceRadix == 10)
-            num = Double.parseDouble(second);
-        else {
-            final String[] parts = second.split("\\.");
-            num = Integer.parseInt(parts[0], sourceRadix);
-            if (parts.length > 1) {
-                int count = 1;
-                for (final String ch : parts[1].split("")) {
-                    final double d = Integer.parseInt(ch, sourceRadix);
-                    num += d / Math.pow(sourceRadix, count);
-                    count++;
-                }
-            }
-        }
         final String third = scanner.nextLine();
-        if (validRadix(third))
-            radix = Integer.parseInt(third);
-        else
-            throw new Exception();
+        scanner.close();
+        sourceRadix = getRadix(first);
+        destRadix = getRadix(third);
+        switch (sourceRadix) {
+            case 1:
+                number = second.trim().length();
+                break;
+            case 10:
+                number = Double.parseDouble(second);
+                break;
+            default:
+                final String[] parts = second.split("\\.");
+                number = Integer.parseInt(parts[0], sourceRadix);
+                if (parts.length > 1) {
+                    int count = 1;
+                    for (final String ch : parts[1].split("")) {
+                        final double d = Integer.parseInt(ch, sourceRadix);
+                        number += d / Math.pow(sourceRadix, count);
+                        count++;
+                    }
+                }
+        }
     }
 
-    private static boolean validRadix(String string) {
-        try {
-            int num = Integer.parseInt(string);
-            return num > 0 && num < 37;
-        } catch (Exception e) {
-            return false;
-        }
+    private static int getRadix(String string) throws Exception {
+        int num = Integer.parseInt(string);
+        if (num > 0 && num < 37)
+            return num;
+        else
+            throw new Exception();
     }
 }
